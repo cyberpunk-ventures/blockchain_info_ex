53ed9d0dcb3fb616287bf6606c1b629262656f5b
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-05-26 21:17:56 +0300

    Added blocks function

--- lib/blockchain_info.ex
+++ lib/blockchain_info.ex
@@ -5,15 +5,27 @@ defmodule BlockchainInfo do
   Blockchain.info endpoints wrapped with HTTPoison
   """
 
-  defp process_response_body(body) do
-    body
-  end
-
   def bitcoins_in_circulation do
-    {:ok, res} = BlockchainInfo.get("https://blockchain.info/q/totalbc")
+    {:ok, res} = BlockchainInfo.get("/q/totalbc")
     res.body
     |> String.to_integer
     |> div(100000000)
   end
+
+  def blocks do
+     {:ok, res} = BlockchainInfo.get("/blocks?format=json")
+     res.body
+     |> Poison.parse!
+     |> Map.get("blocks")
+  end
+
+  defp process_response_body(body) do
+    body
+  end
+
+  defp process_url(path) do
+     "https://blockchain.info" <> path
+  end
+
 
 end
